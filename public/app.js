f986c34fc6308143e16b16deaa2afebbf43978bb
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-22 20:58:15 +0100

    add class invoice test

--- public/app.js
+++ public/app.js
@@ -1,4 +1,22 @@
 "use strict";
+//classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const inv1 = new Invoice("mario", "Work on the mario website", 250);
+const inv2 = new Invoice("luigi", "Work on the luigi website", 300);
+let invoices = [];
+invoices.push(inv1);
+invoices.push(inv2);
+console.log(invoices);
+//inputs
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const toFrom = document.querySelector("#toFrom");
